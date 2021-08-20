8ed84c22c0cea69d7dffdd6109a6c4f1719fb58f
Abderrahmenla <abdo.vic@hotmail.com>
2021-08-20 09:47:01 +0100

    adding the param converter and the delete route

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,41 +3,51 @@
 namespace App\Controller;
 
 use App\Entity\BlogPost;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\Serializer\Serializer;
+
 /**
  * @Route("/blog")
  */
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/{page}",name="blog_list",requirements={"page"="\d+"})
+     * @Route("/{page}",name="blog_list",defaults={"page": 5},requirements={"page"="\d+"})
      */
-    public function list($page=1,Request $request){
-        $limit = $request->get('limit',10);
+    public function list($page = 1, Request $request)
+    {
+        $limit = $request->get('limit', 10);
         $repository = $this->getDoctrine()->getRepository(BlogPost::class);
         $items = $repository->findAll();
-        return $this->json([
-            'page'=>$page,
-            'limit' => $limit,
-            'data'=>array_map(function(BlogPost $item){$this->generateUrl('blog_by_slug',['slug'=>$item->getSlug()]);},$items)
-        ]);
+
+        return $this->json(
+            [
+                'page' => $page,
+                'limit' => $limit,
+                'data' => array_map(function (BlogPost $item) {
+                    return $this->generateUrl('blog_by_slug', ['slug' => $item->getSlug()]);
+                }, $items)
+            ]
+        );
     }
     /**
-     * @Route("/post/{id}",name="blog_by_id",requirements={"id"="\d+"})
+     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"}, methods={"GET"})
      */
-    public function post(BlogPost $post){
+    public function post(BlogPost $post)
+    {
         return $this->json($post);
     }
     /**
-     * @Route("/post/{slug}",name="blog_by_slug")
+     * @Route("/post/{slug}",name="blog_by_slug",methods={"GET"})
      */
 
-    public function postBySlug(BlogPost $slug){
-        return $this->json($slug);
+    public function postBySlug(BlogPost $post){
+        return $this->json($post);
     }
     /**
      * @Route("/add",name="blog_add",methods={"POST"})
@@ -49,5 +59,16 @@ class BlogController extends AbstractController
         $dm->persist($blogPost);
         $dm->flush();
         return $this->json($blogPost);
+    }
+
+    /**
+     * @Route("/post/{id}",name="blog_delete",methods={"DELETE"})
+     */
+    public function delete(BlogPost $post){
+        $dm=$this->getDoctrine()->getManager();
+        $dm->remove($post);
+        $dm->flush();
+
+        return new JsonResponse(null,Response::HTTP_NO_CONTENT);
     }
 }
